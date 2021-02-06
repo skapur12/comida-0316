8a42c0b3062387a51ffab1d2f5b2397d1d08ace9
sanjana <skapur29@gmail.com>
2021-02-06 17:58:50 -0500

    Created user profile that shows user's name and email address upon login

--- app/src/main/java/com/teamcomida/comida_0316/MainActivity.java
+++ app/src/main/java/com/teamcomida/comida_0316/MainActivity.java
@@ -1,19 +1,47 @@
 package com.teamcomida.comida_0316;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
+    TextView fullName, email;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fullName = findViewById(R.id.profileName);
+        email = findViewById(R.id.profileEmail);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                fullName.setText(value.getString("fName"));
+                email.setText(value.getString("email"));
+
+            }
+        });
     }
 
     public void logout (View view) {
