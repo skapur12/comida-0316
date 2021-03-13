49d719fa80e0370cd23e7471a9e454c6889b8a9e
sanjana <skapur29@gmail.com>
2021-03-12 23:03:03 -0500

    Added home, profile, and search buttons to dining hall search screen

--- app/src/main/java/com/teamcomida/comida_0316/SearchDining_2.java
+++ app/src/main/java/com/teamcomida/comida_0316/SearchDining_2.java
@@ -2,13 +2,48 @@ package com.teamcomida.comida_0316;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SearchDining_2 extends AppCompatActivity {
+    ImageView homeButton4, searchButton4, profileButton4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_dining_2);
+
+        homeButton4 = findViewById(R.id.homeButton4);
+        searchButton4 = findViewById(R.id.searchButton4);
+        profileButton4 = findViewById(R.id.profileButton4);
+
+        //Search, Profile, and Home Buttons
+
+        //Search
+        searchButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CollegeSearch.class));
+            }
+        });
+
+        //User Profile
+        profileButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        //Home Button
+        homeButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), HomePage.class));
+            }
+        });
+
     }
 }
